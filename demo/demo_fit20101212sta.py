77a9a1b10deb3e12553153694117938b7bce3ce7
Alexey Isavnin <alexey.isavnin@gmail.com>
2016-08-03 18:41:52 +0300

    insitu fits

--- demo/demo_fit20101212sta.py
+++ demo/demo_fit20101212sta.py
@@ -1,9 +1,12 @@
 
 from ai.fri3d.optimize import fit2remote, fit2insitu
+from ai.fri3d import Evolution
 from astropy import units as u
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
 from ai.shared.data import getSTA
+from matplotlib import pyplot as plt
+from ai.shared.color import BLIND_PALETTE
 
 u.nT = u.def_unit('nT', 1e-9*u.T)
 
@@ -75,7 +78,7 @@ def demo_fit2insitu():
             ]), t
         ),
         poloidal_height=np.array([
-            u.au.to(u.m, [0.1, 0.3])
+            u.au.to(u.m, [0.02, 0.2])
         ]), 
         half_width=np.array([
             u.deg.to(u.rad, [40.0, 70.0])
@@ -106,4 +109,115 @@ def demo_fit2insitu():
         verbose=True,
         timestamp_mask=None)
 
-demo_fit2insitu()
+def demo_insitu(
+    t0=1292253600.0,
+    period=3.0*24.0*3600.0,
+    step=600.0,
+    latitude=lambda t: -5.76484870e-04,
+    longitude=lambda t: 1.08243448e+00,
+    toroidal_height=lambda t: np.polyval(
+        np.array([
+            u.Unit('km/s').to(u.Unit('m/s'), 480.0), 
+            u.au.to(u.m, 0.5)
+        ]), 
+        t
+    ),
+    poloidal_height=lambda t: 1.52375410e+10,
+    half_width=lambda t: 9.24032770e-01,
+    tilt=lambda t: 1.64470079e-02,
+    flattening=lambda t: 4.57895263e-01,
+    pancaking=lambda t: 4.83729672e-01,
+    skew=lambda t: 0.0,
+    twist=lambda t: 4.12708116e+00,
+    flux=lambda t: 4.29414200e+14,
+    sigma=lambda t: 2.0,
+    polarity=-1.0,
+    chirality=1.0,
+    spline_s_phi_kind='cubic',
+    spline_s_phi_n=500):
+
+    d1 = datetime(2010, 12, 15, 10, 20)
+    d2 = datetime(2010, 12, 16, 4)
+    dd = d2-d1
+
+    t, b, p = getSTA(
+        # datetime(2010, 12, 15, 19)-timedelta(hours=12),
+        # datetime(2010, 12, 15, 19)+timedelta(hours=12)
+        d1-dd*0.8,
+        d2+dd*0.8
+    )
+
+    evo = Evolution(
+        latitude=latitude,
+        longitude=longitude,
+        toroidal_height=toroidal_height,
+        poloidal_height=poloidal_height,
+        half_width=half_width,
+        tilt=tilt,
+        flattening=flattening,
+        pancaking=pancaking,
+        skew=skew,
+        twist=twist,
+        flux=flux,
+        sigma=sigma,
+        polarity=polarity,
+        chirality=chirality,
+        spline_s_phi_kind=spline_s_phi_kind,
+        spline_s_phi_n=spline_s_phi_n)
+
+    tt = np.arange(0.0, period+step, step)
+
+    bb = evo.insitu(
+        tt, 
+        x=np.mean(p[:,0]),
+        y=np.mean(p[:,1]),
+        z=np.mean(p[:,2])
+    )
+
+    nonzero_indices = np.nonzero(np.sqrt(bb[:,0]**2+bb[:,1]**2+bb[:,2]**2))[0]
+
+    if nonzero_indices.size >= 2:
+        tt = tt[nonzero_indices[0]:nonzero_indices[-1]+1]
+        bb = bb[nonzero_indices[0]:nonzero_indices[-1]+1,:]
+
+    tt = np.array([datetime.fromtimestamp(tt) for tt in tt+t0+600])
+    bb = u.T.to(u.nT, bb)
+
+    b = u.T.to(u.nT, b)
+
+    fig = plt.figure()
+    plt.plot(t, np.sqrt(b[:,0]**2+b[:,1]**2+b[:,2]**2), 'k')
+    plt.plot(tt, np.sqrt(bb[:,0]**2+bb[:,1]**2+bb[:,2]**2), color=BLIND_PALETTE['reddish-purple'], linewidth=4, linestyle='dashed')
+    plt.plot(t, b[:,0], color=BLIND_PALETTE['vermillion'])
+    plt.plot(tt, bb[:,0], color=BLIND_PALETTE['reddish-purple'], linewidth=4, linestyle='dashed')
+    plt.plot(t, b[:,1], color=BLIND_PALETTE['bluish-green'])
+    plt.plot(tt, bb[:,1], color=BLIND_PALETTE['reddish-purple'], linewidth=4, linestyle='dashed')
+    plt.plot(t, b[:,2], color=BLIND_PALETTE['blue'])
+    plt.plot(tt, bb[:,2], color=BLIND_PALETTE['reddish-purple'], linewidth=4, linestyle='dashed')
+    plt.show()
+
+#             remote  insitu
+# theta        -14.5     0.0
+# varphi        55.0    62.0
+# Rp                     0.1
+# varphi_hw     55.0    52.9
+# gamma         16.0     0.9
+# n              0.6     0.46
+# theta_p       23.0    27.7
+# tau                    4.1
+# Phi                 4.3e14
+
+# demo_fit2remote()
+
+# Run 01.08
+# [ -1.39915561e-02   9.03661698e-01   1.49928626e+10   1.19992133e+00
+#    3.39431978e-02   5.59760053e-01   3.99451581e-01   6.22890240e+00
+#    3.35992244e+14]
+# Run 02.08
+# 1292253600.0
+# [ -5.76484870e-04   1.08243448e+00   1.52375410e+10   9.24032770e-01
+#    1.64470079e-02   4.57895263e-01   4.83729672e-01   4.12708116e+00
+#    4.29414200e+14]
+# demo_fit2insitu()
+
+demo_insitu()
--- demo/demo_fit20111001stb.py
+++ demo/demo_fit20111001stb.py
@@ -1,9 +1,13 @@
 
 from ai.fri3d.optimize import fit2remote, fit2insitu
+from ai.fri3d import Evolution
 from astropy import units as u
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
 from ai.shared.data import getSTB
+from matplotlib import pyplot as plt
+import ai.cdas as cdas
+from ai.shared.color import BLIND_PALETTE
 
 u.nT = u.def_unit('nT', 1e-9*u.T)
 
@@ -51,7 +55,8 @@ def demo_fit2remote():
 
 def demo_fit2insitu():
     t, b, p = getSTB(
-        datetime(2011, 10, 4, 2),
+        # datetime(2011, 10, 4, 2),
+        datetime(2011, 10, 4, 3, 30),
         datetime(2011, 10, 4, 12, 40)
     )
 
@@ -66,7 +71,7 @@ def demo_fit2insitu():
             u.deg.to(u.rad, [-5.0, 10.0])
         ]),
         longitude=np.array([
-            u.deg.to(u.rad, [-110.0, -80.0])
+            u.deg.to(u.rad, [-110.0, -70.0])
         ]), 
         toroidal_height = lambda t: np.polyval(
             np.array([
@@ -76,7 +81,8 @@ def demo_fit2insitu():
             t
         ),
         poloidal_height=np.array([
-            u.au.to(u.m, [0.1, 0.3])
+            u.Unit('km/s').to(u.Unit('m/s'), [0.0, 40.0]),
+            u.au.to(u.m, [0.02, 0.2])
         ]), 
         half_width=np.array([
             u.deg.to(u.rad, [60.0, 90.0])
@@ -107,4 +113,117 @@ def demo_fit2insitu():
         verbose=True,
         timestamp_mask=None)
 
-demo_fit2insitu()
+def demo_insitu(
+    t0=1317556200.0,
+    period=3.0*24.0*3600.0,
+    step=600.0,
+    latitude=lambda t: 2.70502968e-02,
+    longitude=lambda t: -1.33790814e+00,
+    toroidal_height=lambda t: np.polyval(
+        np.array([
+            u.Unit('km/s').to(u.Unit('m/s'), 680.0), 
+            u.au.to(u.m, 0.5)
+        ]), 
+        t
+    ),
+    poloidal_height=lambda t: 1.22125871e+10,
+    half_width=lambda t: 1.28981669e+00,
+    tilt=lambda t: 2.18918189e-01,
+    flattening=lambda t: 7.44157800e-01,
+    pancaking=lambda t: 5.52014484e-01,
+    skew=lambda t: 0.0,
+    twist=lambda t: 1.20193832e+00,
+    flux=lambda t: 5.88473470e+14,
+    sigma=lambda t: 2.0,
+    polarity=1.0,
+    chirality=-1.0,
+    spline_s_phi_kind='cubic',
+    spline_s_phi_n=500):
+
+    d1 = datetime(2011, 10, 4, 3, 30)
+    d2 = datetime(2011, 10, 4, 12, 40)
+    dd = d2-d1
+
+    t, b, p = getSTB(
+        # datetime(2011, 10, 4, 2),
+        # datetime(2011, 10, 4, 8)-timedelta(hours=12),
+        # datetime(2011, 10, 4, 8)+timedelta(hours=12)
+        d1-dd*0.8,
+        d2+dd*0.8
+    )
+
+    evo = Evolution(
+        latitude=latitude,
+        longitude=longitude,
+        toroidal_height=toroidal_height,
+        poloidal_height=poloidal_height,
+        half_width=half_width,
+        tilt=tilt,
+        flattening=flattening,
+        pancaking=pancaking,
+        skew=skew,
+        twist=twist,
+        flux=flux,
+        sigma=sigma,
+        polarity=polarity,
+        chirality=chirality,
+        spline_s_phi_kind=spline_s_phi_kind,
+        spline_s_phi_n=spline_s_phi_n)
+
+    tt = np.arange(0.0, period+step, step)
+
+    bb = evo.insitu(
+        tt, 
+        x=np.mean(p[:,0]),
+        y=np.mean(p[:,1]),
+        z=np.mean(p[:,2])
+    )
+
+    nonzero_indices = np.nonzero(np.sqrt(bb[:,0]**2+bb[:,1]**2+bb[:,2]**2))[0]
+
+    if nonzero_indices.size >= 2:
+        tt = tt[nonzero_indices[0]:nonzero_indices[-1]+1]
+        bb = bb[nonzero_indices[0]:nonzero_indices[-1]+1,:]
+
+    tt = np.array([datetime.fromtimestamp(tt) for tt in tt+t0])
+    bb = u.T.to(u.nT, bb)
+
+    b = u.T.to(u.nT, b)
+
+    fig = plt.figure()
+    plt.plot(t, np.sqrt(b[:,0]**2+b[:,1]**2+b[:,2]**2), 'k')
+    plt.plot(tt, np.sqrt(bb[:,0]**2+bb[:,1]**2+bb[:,2]**2), color=BLIND_PALETTE['reddish-purple'], linewidth=4, linestyle='dashed')
+    plt.plot(t, b[:,0], color=BLIND_PALETTE['vermillion'])
+    plt.plot(tt, bb[:,0], color=BLIND_PALETTE['reddish-purple'], linewidth=4, linestyle='dashed')
+    plt.plot(t, b[:,1], color=BLIND_PALETTE['bluish-green'])
+    plt.plot(tt, bb[:,1], color=BLIND_PALETTE['reddish-purple'], linewidth=4, linestyle='dashed')
+    plt.plot(t, b[:,2], color=BLIND_PALETTE['blue'])
+    plt.plot(tt, bb[:,2], color=BLIND_PALETTE['reddish-purple'], linewidth=4, linestyle='dashed')
+    plt.show()
+
+
+#             remote  insitu
+# theta          5.5     1.6
+# varphi       -95.0   -76.7
+# Rp                    0.08
+# varphi_hw     75.0    73.9
+# gamma         21.0    12.5
+# n              0.55    0.74
+# theta_p       27.0    31.6
+# tau                    1.2
+# Phi                 5.9e14
+
+# demo_fit2remote()
+
+# Run 01.08
+# [ -1.41151664e-02  -1.41494278e+00   1.54493304e+10   1.16863167e+00
+#    1.68696879e-01   6.16380124e-01   4.15676262e-01   1.93219497e+00
+#    5.57677769e+14]
+# Run 02.08
+# 1317556200.0
+# [  2.70502968e-02  -1.33790814e+00   1.22125871e+10   1.28981669e+00
+#    2.18918189e-01   7.44157800e-01   5.52014484e-01   1.20193832e+00
+#    5.88473470e+14]
+# demo_fit2insitu()
+
+demo_insitu()
