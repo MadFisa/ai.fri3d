dd8aaee3243fe760cedc887c7cac297c8d798f8f
Alexey Isavnin <alexey.isavnin@gmail.com>
2017-11-03 17:50:29 +0100

    FRi3D class refactoring

--- ai/fri3d/__init__.py
+++ ai/fri3d/__init__.py
@@ -1,4 +1,7 @@
-
+"""Defines the FRi3D class which describes CME structure and Evolution
+class which handles its evolution.
+"""
+import math
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 from scipy.integrate import fixed_quad
@@ -12,21 +15,146 @@ class FRi3D:
     # pylint: disable=too-many-instance-attributes
     # pylint: disable=E1101
     def __init__(self, **kwargs):
-        self.latitude = kwargs.get('latitude', u.deg.to(u.rad, 0.0))
-        self.longitude = kwargs.get('longitude', u.deg.to(u.rad, 0.0))
-        self.toroidal_height = kwargs.get('toroidal_height', u.au.to(u.m, 1.0))
+        self._latitude = None
+        self._longitude = None
+        self._toroidal_height = None
+        self._poloidal_height = None
+        self._half_width = None
+        self._coeff_angle = None
+        self._tilt = None
+        self._flattening = None
+        self._pancaking = None
+        self._skew = None
+        self._twist = None
+        self._flux = None
+        self._sigma = None
+        self._polarity = None
+        self._chirality = None
+        self.latitude = kwargs.get('latitude', u.deg.to(u.rad, 0))
+        self.longitude = kwargs.get('longitude', u.deg.to(u.rad, 0))
+        self.toroidal_height = kwargs.get('toroidal_height', u.au.to(u.m, 1))
         self.poloidal_height = kwargs.get('poloidal_height', u.au.to(u.m, 0.2))
-        self.half_width = kwargs.get('half_width', u.deg.to(u.rad, 40.0))
-        self.tilt = kwargs.get('tilt', u.deg.to(u.rad, 0.0))
+        self.half_width = kwargs.get('half_width', u.deg.to(u.rad, 40))
+        self.tilt = kwargs.get('tilt', u.deg.to(u.rad, 0))
         self.flattening = kwargs.get('flattening', 0.5)
-        self.pancaking = kwargs.get('pancaking', u.deg.to(u.rad, 20.0))
-        self.skew = kwargs.get('skew', u.deg.to(u.rad, 0.0))
-        self.twist = kwargs.get('twist', 1.0)
+        self.pancaking = kwargs.get(
+            'pancaking',
+            np.arctan(self.poloidal_height/self.toroidal_height)
+        )
+        self.skew = kwargs.get('skew', u.deg.to(u.rad, 0))
+        self.twist = kwargs.get('twist', 1)
         self.flux = kwargs.get('flux', 5e14)
-        self.sigma = kwargs.get('sigma', 2.0)
-        self.polarity = kwargs.get('polarity', 1.0)
-        self.chirality = kwargs.get('chirality', 1.0)
+        self.sigma = kwargs.get('sigma', 2)
+        self.polarity = kwargs.get('polarity', 1)
+        self.chirality = kwargs.get('chirality', 1)
         self.init()
+
+    @property
+    def latitude(self):
+        """Get orientation latitude."""
+        return self._latitude
+    @latitude.setter
+    def latitude(self, latitude):
+        """Set orientation latitude."""
+        self._latitude = subtract_period(latitude, np.pi*2)
+
+    @property
+    def longitude(self):
+        """Get orientation longitude."""
+        return self._longitude
+    @longitude.setter
+    def longitude(self, longitude):
+        """Set orientation longitude."""
+        self._longitude = subtract_period(longitude, np.pi*2)
+
+    @property
+    def toroidal_height(self):
+        """Get toroidal height."""
+        return self._toroidal_height
+    @toroidal_height.setter
+    def toroidal_height(self, toroidal_height):
+        """Set toroidal height."""
+        if toroidal_height > 0:
+            self._toroidal_height = toroidal_height
+        else:
+            raise ValueError('Toroidal height should be positive.')
+
+    @property
+    def poloidal_height(self):
+        """Get poloidal height."""
+        return self._poloidal_height
+    @poloidal_height.setter
+    def poloidal_height(self, poloidal_height):
+        """Set poloidal height."""
+        if poloidal_height > 0:
+            self._poloidal_height = poloidal_height
+        else:
+            raise ValueError('Poloidal height should be positive.')
+
+    @property
+    def half_width(self):
+        """Get half width."""
+        return self._half_width
+    @half_width.setter
+    def half_width(self, half_width):
+        """Set half width explicitly and width coefficient implicitly.
+        """
+        if half_width > 0 and half_width < np.pi*2:
+            self._half_width = half_width
+            self._coeff_angle = np.pi/2/self.half_width
+        else:
+            raise ValueError('Half width should positive and less than 2pi.')
+
+    @property
+    def coeff_angle(self):
+        """Get width coefficient."""
+        return self._coeff_angle
+
+    @property
+    def tilt(self):
+        """Get tilt."""
+        return self._tilt
+    @tilt.setter
+    def tilt(self, tilt):
+        """Set tilt."""
+        self._tilt = subtract_period(tilt, np.pi*2)
+
+    @property
+    def flattening(self):
+        """Get flattening coefficient."""
+        return self._flattening
+    @flattening.setter
+    def flattening(self, flattening):
+        """Set flattening coefficient."""
+        if flattening > 0 and flattening < 1:
+            self._flattening = flattening
+        else:
+            raise ValueError(
+                'Flattening should be greater than 0 and less than 1.'
+            )
+
+    @property
+    def pancaking(self):
+        """Get pancaking (a.k.a., half height)."""
+        return self._pancaking
+    @pancaking.setter
+    def pancaking(self, pancaking):
+        """Set pancaking (a.k.a., half height)."""
+        if pancaking > 0 and pancaking < np.pi:
+            self._pancaking = pancaking
+        else:
+            raise ValueError(
+                'Pancaking should be greater than 0 and less than pi.'
+            )
+
+    @property
+    def skew(self):
+        """Get skewing angle."""
+        return self._skew
+    @skew.setter
+    def skew(self, skew):
+        """Set skewing angle."""
+        self._skew = skew
 
     @property
     def twist(self):
@@ -37,109 +165,9 @@ class FRi3D:
         """Set twist."""
         if twist >= 0.0:
             self._twist = twist
-
-    @property
-    def toroidal_height(self):
-        """Get toroidal height."""
-        return self._toroidal_height
-    @toroidal_height.setter
-    def toroidal_height(self, toroidal_height):
-        """Set toroidal height."""
-        if toroidal_height > 0.0:
-            self._toroidal_height = toroidal_height
-
-    @property
-    def poloidal_height(self):
-        """Get poloidal height."""
-        return self._poloidal_height
-    @poloidal_height.setter
-    def poloidal_height(self, poloidal_height):
-        """Set poloidal height."""
-        if poloidal_height > 0.0:
-            self._poloidal_height = poloidal_height
-
-    @property
-    def half_width(self):
-        """Get half width."""
-        return self._half_width
-    @half_width.setter
-    def half_width(self, half_width):
-        """Set half width explicitly and width coefficient implicitly.
-        """
-        if half_width > 0.0 and half_width < np.pi*2.0:
-            self._half_width = half_width
-            self._coeff_angle = np.pi/2.0/self.half_width
-
-    @property
-    def coeff_angle(self):
-        """Get width coefficient."""
-        return self._coeff_angle
-
-    @property
-    def flattening(self):
-        """Get flattening coefficient."""
-        return self._flattening
-    @flattening.setter
-    def flattening(self, flattening):
-        """Set flattening coefficient."""
-        if flattening > 0.0 and flattening < 1.0:
-            self._flattening = flattening
-
-    @property
-    def pancaking(self):
-        """Get pancaking (a.k.a., half height)."""
-        return self._pancaking
-    @pancaking.setter
-    def pancaking(self, pancaking):
-        """Set pancaking (a.k.a., half height)."""
-        if pancaking is None:
-            self._pancaking = np.arctan(
-                self.poloidal_height/self.toroidal_height
-            )
-        elif pancaking > 0.0 and pancaking < np.pi:
-            self._pancaking = pancaking
-
-    @property
-    def skew(self):
-        """Get skewing angle."""
-        return self._skew
-    @skew.setter
-    def skew(self, skew):
-        """Set skewing angle."""
-        if skew is None:
-            self._skew = 0.0
-        elif skew >= 0.0:
-            self._skew = skew
-
-    @property
-    def latitude(self):
-        """Get orientation latitude."""
-        return self._latitude
-    @latitude.setter
-    def latitude(self, latitude):
-        """Set orientation latitude."""
-        if latitude >= -np.pi/2.0 and latitude <= np.pi/2.0:
-            self._latitude = latitude
-
-    @property
-    def longitude(self):
-        """Get orientation longitude."""
-        return self._longitude
-    @longitude.setter
-    def longitude(self, longitude):
-        """Set orientation longitude."""
-        if longitude >= -np.pi and longitude <= np.pi:
-            self._longitude = longitude
-
-    @property
-    def tilt(self):
-        """Get tilt."""
-        return self._tilt
-    @tilt.setter
-    def tilt(self, tilt):
-        """Set tilt."""
-        if tilt >= -np.pi and tilt <= np.pi:
-            self._tilt = tilt
+        else:
+            self._twist = -twist
+            self._chirality *= -1
 
     @property
     def flux(self):
@@ -148,8 +176,10 @@ class FRi3D:
     @flux.setter
     def flux(self, flux):
         """Set magnetic flux."""
-        if flux > 0.0:
+        if flux > 0:
             self._flux = flux
+        else:
+            raise ValueError('Flux should be positive.')
 
     @property
     def sigma(self):
@@ -160,8 +190,10 @@ class FRi3D:
     def sigma(self, sigma):
         """Set sigma for Gaussian distribution of total magnetic field.
         """
-        if sigma > 0.0:
+        if sigma > 0:
             self._sigma = sigma
+        else:
+            raise ValueError('Sigma should be positive.')
 
     @property
     def polarity(self):
@@ -170,8 +202,10 @@ class FRi3D:
     @polarity.setter
     def polarity(self, polarity):
         """Set polarity."""
-        if polarity == 1.0 or polarity == -1.0:
+        if polarity == 1 or polarity == -1:
             self._polarity = polarity
+        else:
+            raise ValueError('Polarity should be +1 or -1.')
 
     @property
     def chirality(self):
@@ -180,31 +214,45 @@ class FRi3D:
     @chirality.setter
     def chirality(self, chirality):
         """Set chirality."""
-        if chirality == 1.0 or chirality == -1.0:
+        if chirality == 1 or chirality == -1:
             self._chirality = chirality
+        else:
+            raise ValueError('Chirality should be +1 or -1.')
 
     def _vanilla_axis_r(
             self,
             phi,
-            toroidal_height=self.toroidal_height,
-            coeff_angle=self.coeff_angle,
-            flattening=self.flattening):
+            toroidal_height=None,
+            coeff_angle=None,
+            flattening=None):
         """Evaluate the axis function r(phi) in polar coordinates. Note
         that rotational skewing is not taken into account.
         """
+        if toroidal_height is None:
+            toroidal_height = self.toroidal_height
+        if coeff_angle is None:
+            coeff_angle = self.coeff_angle
+        if flattening is None:
+            flattening = self.flattening
         return toroidal_height*np.cos(coeff_angle*phi)**flattening
 
     def _vanilla_axis_dr(
             self,
             phi,
-            toroidal_height=self.toroidal_height,
-            coeff_angle=self.coeff_angle,
-            flattening=self.flattening):
+            toroidal_height=None,
+            coeff_angle=None,
+            flattening=None):
         """Evaluate the derivative of the axis function dr/d(phi). Note
         that rotational skewing is not taken into account.
         """
+        if toroidal_height is None:
+            toroidal_height = self.toroidal_height
+        if coeff_angle is None:
+            coeff_angle = self.coeff_angle
+        if flattening is None:
+            flattening = self.flattening
         return(
-            -flattening*coeff_angle*np.tan(coeff_angle*phi)
+            (-1)*flattening*coeff_angle*np.tan(coeff_angle*phi)
             *self._vanilla_axis_r(
                 phi,
                 toroidal_height=toroidal_height,
@@ -215,12 +263,18 @@ class FRi3D:
     def _vanilla_axis_ds(
             self,
             phi,
-            toroidal_height=self.toroidal_height,
-            coeff_angle=self.coeff_angle,
-            flattening=self.flattening):
+            toroidal_height=None,
+            coeff_angle=None,
+            flattening=None):
         """Evaluate derivative of the axis length ds/d(phi). Note that
         rotational skewing is not taken into account.
         """
+        if toroidal_height is None:
+            toroidal_height = self.toroidal_height
+        if coeff_angle is None:
+            coeff_angle = self.coeff_angle
+        if flattening is None:
+            flattening = self.flattening
         return(
             self._vanilla_axis_r(
                 phi,
@@ -302,7 +356,7 @@ class FRi3D:
                         -np.pi/2*ratio,
                         np.pi/2*ratio,
                         n=1000
-                    )[0]-_self._vanilla_axis_r(
+                    )[0]-self._vanilla_axis_r(
                         coeff_angle_phi/coeff_angle,
                         toroidal_height=1,
                         coeff_angle=coeff_angle,
@@ -733,3 +787,6 @@ class Evolution:
         bmap = np.reshape(bmap, [dx.size, dy.size])
 
         return bmap.T
+
+def subtract_period(value, period):
+    return value-math.copysign(value, 1)*(math.fabs(value)//period)*period
