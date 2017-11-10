4aab4b0bb11085041f207780f5d36a0f074adf19
Alexey Isavnin <alexey.isavnin@gmail.com>
2017-11-10 16:16:08 +0100

    Designing the profile types

--- ai/fri3d/fri3d_fit_insitu.py
+++ ai/fri3d/fri3d_fit_insitu.py
@@ -6,4 +6,36 @@ class FRi3DFitInSitu(BaseFRi3DFit):
         super(FRi3DFitInSitu, self).__init__(DynamicFRi3D, **kwargs)
 
     def fit(self, **kwargs):
-        pass
+        def residual(**kwargs):
+            pass
+
+class BaseProfile:
+    def __init__(self, profile):
+        self._profile = profile
+
+    @property
+    def profile(self):
+        return self._profile
+
+    def eval(self, t):
+        raise NotImplementedError
+
+class PolyProfile(BaseProfile):
+    def __init__(self, params, bounds):
+        super(PolyProfile, self).__init__('poly')
+        self._params = params
+        self._bounds = bounds
+
+    def eval(self, t):
+        np.polyval(self._params, t)
+    
+    @property
+    def params(self):
+        return self._params
+
+    @property
+    def bounds(self):
+        return self._bounds
+
+class ExpProfile(BaseProfile):
+    pass
