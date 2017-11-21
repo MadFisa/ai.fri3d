4c6a593ba705f4a0d608378320bd5369702c2b8a
Alexey Isavnin <alexey.isavnin@gmail.com>
2017-11-21 17:22:29 +0100

    In-situ fitting works

--- ai/fri3d/optimize.py
+++ ai/fri3d/optimize.py
@@ -6,24 +6,39 @@ white-light and in-situ data.
 # pylint: disable=E0401
 # pylint: disable=C0103
 # pylint: disable=W0212
+from datetime import datetime
 import numpy as np
 from scipy.interpolate import interp1d
 from scipy.optimize import differential_evolution
 from scipy.spatial.distance import euclidean
+from matplotlib import pyplot as plt
 from astropy import units as u
 from fastdtw import fastdtw
 from ai.fri3d.model import StaticFRi3D, DynamicFRi3D
 
-def fit2insitu(t, x, y, z, b, v=None, sampling=50, relative_time=None, **kwargs):
+d_prev = np.inf
+
+def fit2insitu(
+        t, x, y, z, b, v=None,
+        sampling=50, relative=True, verbose=False, **kwargs):
     """Fits FRi3D model to in-situ data (magnetic field and speed).
 
     Args:
         t (np.ndarray): array of timestamps (n)
+        x (float): x-coordinate of the spacecraft
+        y (float): y-coordinate of the spacecraft
+        z (float): z-coordinate of the spacecraft
         b (np.ndarray): array of magnetic field vectors (n, 3)
         v (np.ndarray): array of absolute speed values (n)
+        sampling (int): number of sampling points used for fitting
+        relative (bool): flag for using relative time-profiles (relative
+            to the starting time of the fitted data t[0])
+        **kwargs: keyworded profiles for all model parameters
 
     Returns:
         (DynamicFRi3D) fitted dynamic FRi3D model
+        (dict) dictionary of fitted profiles for all the model
+            parameters
     """
     t_real = np.linspace(t[0], t[-1], sampling)
     dt_real = t_real[1]-t_real[0]
@@ -32,6 +47,7 @@ def fit2insitu(t, x, y, z, b, v=None, sampling=50, relative_time=None, **kwargs)
     mask = np.logical_not(np.any(np.isnan(b_real), axis=1))
     tb_real = t_real[mask]
     b_real = b_real[mask, :]
+    bt_real = np.sqrt(b_real[:, 0]**2+b_real[:, 1]**2+b_real[:, 2]**2)
     if v is not None:
         f = interp1d(t, v, kind='linear', axis=0)
         v_real = f(t_real)
@@ -49,39 +65,39 @@ def fit2insitu(t, x, y, z, b, v=None, sampling=50, relative_time=None, **kwargs)
         else:
             profiles[prop] = kwargs[prop]
             if profiles[prop].bounds is None:
-                if relative_time is None:
-                    setattr(dfr, prop, profiles[prop].eval)
-                else:
+                if relative:
                     setattr(
                         dfr,
                         prop,
-                        lambda t: profiles[prop].eval(t-relative_time)
+                        lambda t, profile=profiles[prop]:
+                            profile.eval(t-t_real[0])
                     )
+                else:
+                    setattr(dfr, prop, profiles[prop].eval)
     def residual(params):
-        print(params)
+        global d_prev
         i = 0
         for prop, profile in profiles.items():
             if profile.bounds is not None:
                 n = len(profile.bounds)
                 profile.params = params[i:i+n]
                 i += n
-                if relative_time is None:
-                    setattr(dfr, prop, profile.eval)
-                else:
+                if relative:
                     setattr(
                         dfr,
                         prop,
-                        lambda t: profile.eval(t-relative_time)
+                        lambda t, profile=profile: profile.eval(t-t_real[0])
                     )
+                else:
+                    setattr(dfr, prop, profile.eval)
         t_model = np.arange(
             t_real[0]-dt_real*sampling,
             t_real[-1]+dt_real*(sampling+1),
             dt_real
         )
         b_model, v_model = dfr.insitu(t_model, x, y, z)
-        nonzero_indices = np.where(np.isfinite(np.sqrt(
-            b_model[:, 0]**2+b_model[:, 1]**2+b_model[:, 2]**2
-        )))[0]
+        bt_model = np.sqrt(b_model[:, 0]**2+b_model[:, 1]**2+b_model[:, 2]**2)
+        nonzero_indices = np.where(np.isfinite(bt_model))[0]
         if nonzero_indices.size >= 2:
             t_model = t_model[nonzero_indices[0]:nonzero_indices[-1]+1]
             if t_model[0] > t_real[-1] or t_model[-1] < t_real[0]:
@@ -89,15 +105,101 @@ def fit2insitu(t, x, y, z, b, v=None, sampling=50, relative_time=None, **kwargs)
             b_model = b_model[nonzero_indices[0]:nonzero_indices[-1]+1, :]
             v_model = v_model[nonzero_indices[0]:nonzero_indices[-1]+1]
             db = fastdtw(
-                np.hstack((np.array([t_model]).T, b_model)),
-                np.hstack((np.array([tb_real]).T, b_real)),
+                np.hstack((
+                    (np.array([t_model]).T-t_real[0])/(t_real[-1]-t_real[0]),
+                    b_model/np.amax(bt_real)
+                )),
+                np.hstack((
+                    (np.array([tb_real]).T-t_real[0])/(t_real[-1]-t_real[0]),
+                    b_real/np.amax(bt_real)
+                )),
                 dist=euclidean
-            )
+            )[0]
             dv = fastdtw(
                 np.vstack((t_model, v_model)).T,
                 np.vstack((tv_real, v_real)).T,
                 dist=euclidean
-            ) if v is not None else 0
+            )[0] if v is not None else 0
+            if verbose and db+dv < d_prev:
+                d_prev = db+dv
+
+                d_real = np.array(
+                    [datetime.fromtimestamp(t) for t in t_real]
+                )
+                d_model = np.array(
+                    [datetime.fromtimestamp(t) for t in t_model]
+                )
+                plt.close('all')
+                fig = plt.figure()
+                plt.subplots_adjust(hspace=0.001)
+                ax1 = fig.add_subplot(211)
+                ax1.plot(
+                    d_real,
+                    np.sqrt(
+                        b_real[:, 0]**2+
+                        b_real[:, 1]**2+
+                        b_real[:, 2]**2
+                    ),
+                    'k'
+                )
+                ax1.plot(
+                    d_real,
+                    b_real[:, 0],
+                    'r'
+                )
+                ax1.plot(
+                    d_real,
+                    b_real[:, 1],
+                    'g'
+                )
+                ax1.plot(
+                    d_real,
+                    b_real[:, 2],
+                    'b'
+                )
+                ax1.plot(
+                    d_model,
+                    np.sqrt(
+                        b_model[:, 0]**2+
+                        b_model[:, 1]**2+
+                        b_model[:, 2]**2
+                    ),
+                    '--k'
+                )
+                ax1.plot(
+                    d_model,
+                    b_model[:, 0],
+                    '--r'
+                )
+                ax1.plot(
+                    d_model,
+                    b_model[:, 1],
+                    '--g'
+                )
+                ax1.plot(
+                    d_model,
+                    b_model[:, 2],
+                    '--b'
+                )
+                # ax2 = fig.add_subplot(212, sharex=ax1)
+                # ax2.plot(
+                #     d_real,
+                #     v_real,
+                #     'k'
+                # )
+                # ax2.plot(
+                #     d_model,
+                #     v_model,
+                #     '--k'
+                # )
+
+                plt.setp(ax1.get_xticklabels(), visible=False)
+                plt.ion()
+                plt.draw()
+                plt.pause(0.1)
+                plt.show()
+
+                print(d_prev, params)
             return db+dv
         else:
             return np.inf
@@ -106,7 +208,6 @@ def fit2insitu(t, x, y, z, b, v=None, sampling=50, relative_time=None, **kwargs)
         if kwargs[prop].bounds is not None:
             for i in range(len(kwargs[prop].bounds)):
                 bounds.append(kwargs[prop].bounds[i])
-    print(bounds)
     res = differential_evolution(residual, bounds=bounds)
     i = 0
     for prop, profile in profiles.items():
--- examples/example_fit2insitu.py
+++ examples/example_fit2insitu.py
@@ -1,15 +1,17 @@
-
+# pylint: disable=E1101
 from datetime import datetime
 import calendar
 import numpy as np
 from scipy.interpolate import interp1d
+import matplotlib
+matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 from astropy import units as u
 from ai import cdas
 from ai.shared.data import getSTA
 from ai.fri3d.optimize import PolyProfile, fit2insitu
 
-# cdas.set_cache(True, './data')
+cdas.set_cache(True, './data')
 # data_b = cdas.get_data(
 #     'sp_phys',
 #     'STA_L1_MAG_RTN',
@@ -19,14 +21,14 @@ from ai.fri3d.optimize import PolyProfile, fit2insitu
 #     cdf=True
 # )
 
-# data_v = cdas.get_data(
-#     'sp_phys',
-#     'STA_L2_PLA_1DMAX_1MIN',
-#     datetime(2010, 12, 15, 10, 20),
-#     datetime(2010, 12, 16, 4),
-#     ['proton_bulk_speed'],
-#     cdf=True
-# )
+data_v = cdas.get_data(
+    'sp_phys',
+    'STA_L2_PLA_1DMAX_1MIN',
+    datetime(2010, 12, 15, 10, 20),
+    datetime(2010, 12, 16, 4),
+    ['proton_bulk_speed'],
+    cdf=True
+)
 
 # m = data_v['proton_bulk_speed'] > 0
 # data_v['epoch'] = data_v['epoch'][m]
@@ -42,6 +44,14 @@ d, b, _, p = getSTA(
     datetime(2010, 12, 16, 4),
 )
 
+# f = interp1d(
+#     data_v['epoch'],
+#     data_v['proton_bulk_speed'],
+#     kind='linear',
+#     axis=0
+# )
+# v = f(d)
+
 # print(v)
 
 t = [calendar.timegm(x.timetuple()) for x in d]
@@ -51,24 +61,25 @@ dfr = fit2insitu(
     np.mean(p[:, 1]),
     np.mean(p[:, 2]),
     b,
+    v=None,
     latitude=PolyProfile(bounds=[u.deg.to(u.rad, [-15, 5]).tolist()]),
-    longitude=PolyProfile(params=u.deg.to(u.rad, [55]).tolist()),
+    longitude=PolyProfile(bounds=[u.deg.to(u.rad, [40, 70]).tolist()]),
     toroidal_height=PolyProfile(bounds=[
         (400e3, 600e3),
         (u.au.to(u.m, 0.5), u.au.to(u.m, 1.2))
     ]),
     poloidal_height=PolyProfile(bounds=[u.au.to(u.m, [0.01, 0.2]).tolist()]),
     half_width=PolyProfile(params=u.deg.to(u.rad, [55]).tolist()),
-    tilt=PolyProfile(bounds=[u.deg.to(u.rad, [10, 30]).tolist()]),
+    tilt=PolyProfile(bounds=[u.deg.to(u.rad, [0, 30]).tolist()]),
     flattening=PolyProfile(bounds=[(0.2, 0.8)]),
-    pancaking=PolyProfile(bounds=[u.deg.to(u.rad, [15, 30]).tolist()]),
+    pancaking=PolyProfile(bounds=[u.deg.to(u.rad, [10, 30]).tolist()]),
     skew=PolyProfile(params=[0]),
     twist=PolyProfile(bounds=[(0, 3)]),
-    flux=PolyProfile(bounds=[(1e13, 1e15)]),
+    flux=PolyProfile(bounds=[(1e14, 1e15)]),
     sigma=PolyProfile(params=[2]),
     polarity=PolyProfile(params=[-1]),
     chirality=PolyProfile(params=[1]),
-    relative_time=t[0]-6*3600
+    verbose=True
 )
 
 # t0
