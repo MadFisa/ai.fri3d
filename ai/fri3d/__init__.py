d227b0356f61c705324059521415428591366677
Alexey Isavnin <alexey.isavnin@gmail.com>
2017-11-24 16:08:48 +0100

    Treat different ai packages as part of one family

--- ai/fri3d/__init__.py
+++ ai/fri3d/__init__.py
@@ -1,8 +1,2 @@
-"""Init module. Loads all the necessary classes into the package
-namespace.
-"""
-# from .model import StaticFRi3D
-# from .dynamic_fri3d import DynamicFRi3D
-# from .base_fri3d_fit import BaseFRi3DFit
-# from .fri3d_fit_cor import FRi3DFitCor
-# from .fri3d_fit_insitu import FRi3DFitInSitu
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
