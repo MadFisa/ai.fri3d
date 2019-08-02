2703346f11e134e0fa8e4309a14eec7132cf8b11
Alexey Isavnin <alexey.isavnin@gmail.com>
2019-08-02 15:09:40 +0300

    Restrusctured the repo

--- setup.py
+++ setup.py
@@ -1,19 +1,37 @@
+import sys
+import subprocess
+import os
+import shutil
 
+# for consistent encoding
+from codecs import open
 from setuptools import setup, find_packages
 
-setup(
-    name='cme',
-    
-    version='1.0.0.dev1',
-    
-    description='The model of Coronal Mass Ejection (CME)',
+version_py = open(os.path.join(os.path.dirname(__file__), "version.py")).read().strip().split("=")[-1].replace('"', "")
 
-    author='Alexey Isavnin',
-    author_email='alexey.isavnin@gmail.com',
-    
-    license='MIT',
-    
-    keywords='cme coronal mass ejection sun solar physics model',
-    
-    packages=find_packages(exclude=['tests*']),
+# Get the long description from the README file
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="ai.cdas",
+    version="{ver}".format(ver=version_py),
+    description="FRi3D model of coronal mass ejections",
+    long_description=long_description,
+    url="https://bitbucket.org/isavnin/ai.fri3d",
+    author="Alexey Isavnin",
+    author_email="alexey.isavnin@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="research space solar physics science model coronal mass ejection flare CME",
+    packages=find_packages("src", exclude=["test*"]),
+    package_dir={"": "src"},
+    install_requires=["numpy", "scipy", "matplotlib", "numba", "python3-tk", "astropy", "fastdtw", "ai.cs"],
+    extras_require={"CDF": ["spacepy"]},
 )
